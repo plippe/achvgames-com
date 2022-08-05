231874aeb687e84d482519d446db04952c4f7d31
Philippe Vinchon <p.vinchon@gmail.com>
2022-08-05 21:08:46 +0100

    Add GraphQL

--- src/bin/web_server.rs
+++ src/bin/web_server.rs
@@ -1,21 +1,36 @@
-use tide::Request;
-use tide::prelude::*;
+use async_graphql::{
+    http::{playground_source, GraphQLPlaygroundConfig},
+    EmptyMutation, EmptySubscription, Object, Schema,
+};
+use tide::{http::mime, Body, Response, StatusCode};
 
-#[derive(Debug, Deserialize)]
-struct Animal {
-    name: String,
-    legs: u16,
+struct QueryRoot;
+
+#[Object]
+impl QueryRoot {
+    async fn foo(&self) -> String {
+        "bar".to_owned()
+    }
 }
 
 #[async_std::main]
 async fn main() -> tide::Result<()> {
-    let mut app = tide::new();
-    app.at("/orders/shoes").post(order_shoes);
-    app.listen("127.0.0.1:8080").await?;
-    Ok(())
-}
+    let schema = Schema::build(QueryRoot, EmptyMutation, EmptySubscription).finish();
 
-async fn order_shoes(mut req: Request<()>) -> tide::Result {
-    let Animal { name, legs } = req.body_json().await?;
-    Ok(format!("Hello, {}! I've put in an order for {} shoes", name, legs).into())
+    let mut app = tide::new();
+
+    app.at("/").get(|_| async move {
+        let mut resp = Response::new(StatusCode::Ok);
+        resp.set_body(Body::from_string(playground_source(
+            GraphQLPlaygroundConfig::new("/graphql"),
+        )));
+        resp.set_content_type(mime::HTML);
+        Ok(resp)
+    });
+
+    app.at("/graphql").post(async_graphql_tide::graphql(schema));
+
+    app.listen("127.0.0.1:8080").await?;
+
+    Ok(())
 }
