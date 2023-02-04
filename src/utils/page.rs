bc985761d69910a3bc41ba6b25452b3bf83beb6b
Philippe Vinchon <p.vinchon@gmail.com>
2023-02-04 00:20:21 +0000

    Bump libraries

--- src/utils/page.rs
+++ src/utils/page.rs
@@ -2,6 +2,8 @@ use crate::steam;
 use chaining::Pipe;
 use async_graphql::OutputType;
 use async_graphql::{scalar, SimpleObject};
+use base64::Engine;
+use base64::engine::general_purpose::URL_SAFE_NO_PAD;
 
 #[derive(SimpleObject)]
 #[graphql(concrete(name = "PageSteamGame", params(steam::Game)))]
@@ -22,13 +24,13 @@ scalar!(Cursor);
 
 impl Cursor {
     pub fn try_into_u32(self) -> Option<u32> {
-        base64::decode_config(self.0, base64::URL_SAFE_NO_PAD)
+        URL_SAFE_NO_PAD.decode(self.0)
             .ok()
             .and_then(|bytes| String::from_utf8(bytes).ok())
             .and_then(|str| str.parse::<u32>().ok())
     }
 
     pub fn from_u32(value: u32) -> Cursor {
-        base64::encode_config(value.to_string(), base64::URL_SAFE_NO_PAD).pipe(Cursor)
+        URL_SAFE_NO_PAD.encode(value.to_string()).pipe(Cursor)
     }
 }
